fe153a66929e28c375d0854edb95d78cfeb042bf
praveenkumar <praveenmech483@gmail.com>
2024-05-29 10:49:40 +0530

    updated

--- src/utils/config.jsx
+++ src/utils/config.jsx
@@ -1,4 +1,4 @@
 export default {
-    API_URL: 'https://inventory-management-be-2.onrender.com/'
+    API_URL: 'https://inventory-management-be-3.onrender.com/'
     // API_URL: 'http://localhost:3001'
 }
